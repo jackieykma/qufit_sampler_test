08d2b4c2a270bb57ffa8d5b68b32fa316135a41b
Yik Ki (Jackie) Ma <yikki.ma@anu.edu.au>
2025-05-28 19:38:47 +0200

    First working version

---
+++ gen_src.py
@@ -0,0 +1,55 @@
+'''
+Generate mock observations of sources
+
+Each source has a certain intrinsic polarisation properties, and each source is further simulated multiple times to take measurement uncertainty into account
+'''
+
+import polsim
+import numpy as np
+import os
+
+
+
+## Define a list of sources to simulate
+## One randomisation seed will produce one simulation realisation
+src_list = [
+{"model_sel": 1, 
+"pDict": {"fracPol": 0.3, "psi0_deg": 55., "RM_radm2": -250.,}, 
+"freq_array": np.arange(800.e6, 1089.e6, 1.e6),
+"iDict": {"reffreq": 944.e6, "flux": 2.e-3, "alpha": 0.},
+"noise": 20.e-6, 
+"seed_list": np.arange(1000, 1011, 1)
+},
+{"model_sel": 11,
+"pDict": {"fracPol1": 0.2, "fracPol2": 0.1, "psi01_deg": 155., "psi02_deg": 30., "RM1_radm2": 400., "RM2_radm2": -250.,},
+"freq_array": np.arange(800.e6, 1089.e6, 1.e6),
+"iDict": {"reffreq": 944.e6, "flux": 2.e-3, "alpha": 0.},
+"noise": 20.e-6,
+"seed_list": np.arange(10000, 10011, 1)
+},
+]
+
+
+
+## A dummy variable to give each source a unique "name"
+nsrc = 0
+
+for src in src_list:
+   ## A dummy variable to give each realisation a unique "name"
+   nreal = 0
+   for seed in src['seed_list']:
+      ## Generate the simulated IQU values
+      i_list, q_list, u_list = polsim.iqu_sim(src["model_sel"], src["pDict"], src["freq_array"], src["iDict"], src["noise"], seed)
+      ## Create the output files, each source / realisation in one dir.
+      os.system('mkdir src'+str(nsrc)+'_real'+str(nreal))
+      f = open('src'+str(nsrc)+'_real'+str(nreal)+'/IQU.tsv', 'w')
+      for _ in range(len(src["freq_array"])):
+         f.write('%.0f' % src["freq_array"][_]+'\t%.8f' % i_list[_]+'\t%.8f' % q_list[_]+'\t%.8f' % u_list[_]+'\t%.8f' % src["noise"]+'\t%.8f' % src["noise"]+'\t%.8f' % src["noise"]+'\n')
+      f.close()
+      nreal += 1
+   nsrc += 1
+
+
+
+
+
---
+++ run_qufit.py
@@ -0,0 +1,69 @@
+'''
+Actual running of QU-fitting
+Only use the correct model for fitting
+'''
+
+
+
+import os
+import numpy as np
+import glob
+
+
+
+def run_qufit(srcname, model, nruns, sampler, qufit_path):
+   for n in range(nruns):
+      if os.path.exists(srcname+'/IQU_m'+str(model)+'_'+str(n)+'_'+sampler+'.json'):
+         print('Skipping for source '+srcname+', nrun='+str(n)+'...')
+      else:
+         print('Running for source '+srcname+', nrun='+str(n)+'...')
+         os.system('python3 '+qufit_path+' '+srcname+'/IQU.tsv -m '+str(model)+' --sampler '+sampler+' --ncores 16 --nlive 128')
+         ## Change name to reflect the nruns
+         os.system('mv '+srcname+'/IQU_m'+str(model)+'_'+sampler+'.dat '+srcname+'/IQU_m'+str(model)+'_'+str(n)+'_'+sampler+'.dat')
+         os.system('mv '+srcname+'/IQU_m'+str(model)+'_'+sampler+'.json '+srcname+'/IQU_m'+str(model)+'_'+str(n)+'_'+sampler+'.json')
+         ## Remove some files --- too costly to save!
+         os.system('rm -rf '+srcname+'/IQUfig_*_corner.pdf')
+         os.system('rm -rf '+srcname+'/IQUfig_*_specfit.pdf')
+         os.system('rm -rf '+srcname+'/IQU_m'+str(model)+'_'+sampler)
+
+
+
+
+if __name__ == "__main__":
+   import argparse
+   parser = argparse.ArgumentParser()
+   parser.add_argument(
+      "-n",
+      dest="srcname",
+      help="Source directory name.",
+      type=str,
+   )
+   parser.add_argument(
+      "-m",
+      dest="model",
+      help="QU-fitting model to attempt (e.g. 1, 2, 11...).",
+      type=int,
+   )
+   parser.add_argument(
+      "-k",
+      dest="nruns",
+      help="Number of QU-fitting attempts.",
+      type=int,
+   )
+   parser.add_argument(
+      "-s",
+      dest="sampler",
+      help="Nestled sampling sampler to use.",
+      type=str,
+   )
+   parser.add_argument(
+      "-p",
+      dest="qufit_path",
+      help="QU-fitting full path (.../.../RM-Tools/RMtools_1D/do_QUfit_1D_mnest.py).",
+      type=str,
+   )
+   
+   args = parser.parse_args()
+   
+   run_qufit(args.srcname, args.model, args.nruns, args.sampler, args.qufit_path)
+
